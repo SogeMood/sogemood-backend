4bda599e88419f4da8f4947c2744cb497ab81898
mwerlen <maxime@werlen.fr>
2015-07-26 19:37:14 +0200

    Setting up project with a test framework

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sogemood-backend',
+    version='1.0',
+    packages=[''],
+    url='sogemood.appspot.com',
+    license='MIT',
+    author='mwerlen',
+    author_email='maxime@werlen.fr',
+    description='SogeMood\'s Backend'
+)
